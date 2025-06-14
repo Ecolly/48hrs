1830637db5c1f5255520cea840cd680448f70975
Faye <feifei123zhang@gmail.com>
2025-06-14 14:52:28 -0600

    changes to item class + map tile assignment

--- game_classes/item.py
+++ game_classes/item.py
@@ -1,31 +1,31 @@
 
 import pyglet
+from enum import Enum, auto
 
 def create_sprite_item(image_grid, index): #dumb. literally the same as the image handling function
     tex = pyglet.image.Texture.create(16, 16)
     tex.blit_into(image_grid[index], 0, 0, 0)
     return pyglet.sprite.Sprite(tex, x=0, y=0)
 
+
 class Item:
     #very basics item class cause we dono what items there are
     def __init__(self, name, grid_items, x, y, quantity):
-
-        item_names = ["Kitchen Knife", "Machete", "Scimitar", "Screwdriver", "Sickle"]
-        item_fakenames = ["Kitchen Knife", "Machete", "Scimitar", "Screwdriver", "Sickle"]
+        # item_names = ["Kitchen Knife", "Machete", "Scimitar", "Screwdriver", "Sickle"]
+        # item_fakenames = ["Kitchen Knife", "Machete", "Scimitar", "Screwdriver", "Sickle"]
         item_spritelocs = [29*10, 29*10+1, 29*10+2, 29*10+3, 29*10+4]
-        item_is_usable = [1, 1, 1, 1, 1]
 
         self.name = name
-        self.index = item_names.index(name)
-        self.fakename = item_fakenames[self.index]
+        # self.index = item_names.index(name)
+        # self.fakename = item_fakenames[self.index]
         self.sprite = create_sprite_item(grid_items, item_spritelocs[self.index])
         self.x = x
         self.y = y
-        self.prevx = x
+        self.prevx = x #previous x and y coordanites, for animating
         self.prevy = y
         self.quantity = quantity
         self.scale = 3
-        self.is_usable = item_is_usable[self.index]
+        self.is_usable = True #default to true
 
 
     def use(self, target):
@@ -63,6 +63,29 @@ class Item:
         #sprite.color = self.color
         sprite.batch = batch
         sprite.z = 30
+
+class Weapon(Item):
+    def __init__(self, name, grid_items, x, y, quantity, damage, durability, is_usable=True):
+        super().__init__(name, grid_items, x, y, quantity)
+        self.damage = damage
+        self.durability = durability  # Default durability
+        self.damage_type = "slashing"  # Default damage type
+        self.is_usable = is_usable  # Default to usable
+
+
+class Consumable(Item):
+    def __init__(self, name, grid_items, x, y, quantity, nutrition_value):
+        super().__init__(name, grid_items, x, y, quantity)
+        self.nutrition_value = nutrition_value
+        self.health_restored = 5  # Default health restored
+
+# class Shield (Item):
+
+# class Staff(Item):
+
+
+# class Potion(Item):
+
 
 
 
--- test.py
+++ test.py
@@ -1,5 +1,27 @@
 from game_classes import map
 import random
+from game_classes.item import Item
+from game_classes.item import Weapon, Consumable
+
+item_definitions = [
+    # name, class, grid_items, x, y, quantity, sprite_index, [extra args...]
+    ("Kitchen Knife", Weapon, 5, 5, 1, 0, 10, 100),  # sprite_index=0
+    ("Health Potion", Consumable, 7, 7, 3, 5, 20),   # sprite_index=5
+    ("Machete", Weapon, 10, 10, 1, 1, 15, 80),       # sprite_index=1
+]
+
+items = []
+#self, name, grid_items, x, y, quantity
+for _ in range(3):  # Generate 3 items
+    definition = random.choice(item_definitions)
+    name, cls, x, y, quantity, sprite_index, *extra = definition
+    if cls is Weapon:
+        item = cls(name, x, y, quantity)
+    elif cls is Consumable:
+        item = cls(name, x, y, quantity)
+    else:
+        item = cls(name, x, y, quantity,)
+    items.append(item)
 
 def make_floor():
     number_of_rooms = random.randint(5, 9)  # Random number of rooms between 5 and 10
